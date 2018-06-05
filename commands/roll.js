7001eb4e92c1911b5f8315b1fc29cdad059fd73a
Anameus <annemie.burger@gmail.com>
2018-06-05 19:27:57 +0200

    replace var keyword with fancy-er and modern let keyword

--- commands/roll.js
+++ commands/roll.js
@@ -1,10 +1,10 @@
 exports.run = (client, message, args) => {
     message.channel.send("You rolled a d10 die " + args + " times!").catch(console.error);
-    var diceType = 10; // edit for any other dice (d20, d12 etc)
-    var diceNum = args; //number of dice to roll
-    var diceArr = []; //array to store rolled die
+    let diceType = 10; // edit for any other dice (d20, d12 etc)
+    let diceNum = args; //number of dice to roll
+    let diceArr = []; //array to store rolled die
     for (var i = 0; i < diceNum; i++) {
-        var rolled = Math.floor(Math.random() * diceType + 1); //diceType plus 1 to make rolling a 10 possible
+        let rolled = Math.floor(Math.random() * diceType + 1); //diceType plus 1 to make rolling a 10 possible
         if (rolled > 0) {
             diceArr.push(rolled); //no zeros allowed...sorry
         }
@@ -13,8 +13,8 @@ exports.run = (client, message, args) => {
         }
     }
     diceArr.sort(function (a, b) { return b - a; }); //sorts the rolled dice
-    var successNum = 0; //var to represent success value
-    var successArr = []; //array to store success values
+    let successNum = 0; //var to represent success value
+    let successArr = []; //array to store success values
     for (var k = 0; k < diceArr.length; k++) {
          switch (diceArr[k]) { //switch statement to calculate success values
             case 10:
